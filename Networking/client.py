617cf8c419ceefcf82d2632480ade2bdeebad2e6
jeslinjoy <jeslin@qburst.com>
2015-07-31 11:46:51 +0530

    Update client.py

--- Networking/client.py
+++ Networking/client.py
@@ -2,7 +2,7 @@ import socket
 
 HOST = '10.5.1.144'    
 PORT = 4444      
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((HOST,PORT))
 with open('myUpload.txt', 'rb') as file_to_send:
     for data in file_to_send:
